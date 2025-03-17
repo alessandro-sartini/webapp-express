c396775370299c58d84643e1460a7d3b33210d05
alessandro-sartini <ale.sartini.98@gmail.com>
2025-03-17 11:53:26 +0100

    ADD:cors

--- app.js
+++ app.js
@@ -2,11 +2,20 @@
 import express from "express"
 import router from "./routers/movieRouter.js";
 import handleImgPath from "./middlewares/handleImgPath.js";
+import cors from 'cors';
 
 const app = express();
 const port = process.env.SERVER_PORT;
 
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+    })
+);
+
+
 app.use(express.static('public'));
+
 //! middlewares immagini
 app.use(handleImgPath);
 
